20ec1d2586a0e4d6c2c08e0db0ff6cafdd603e26
Ruby-sahu <rubysahu1393@gmail.com>
2025-02-25 22:11:49 +0530

    Deluxe room details top sections

--- src/app/offers-menu/page.js
+++ src/app/offers-menu/page.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Header from '../components/Header'
+import RoomGallerySlider from '../components/room-detail/RoomGallerySlider'
+import WildBreadcrumb from "@/app/components/breadcrumb/WildBreadcrumb"
+import DeluxeRoomDetail from '../components/deluxe-room/DeluxeRoomDetail'
+
+
 
 function page() {
   return (
     <div>
-      offers menu
+      <Header />
+      <div className="md:pt-20 pt-16">
+        <WildBreadcrumb />
+      </div>
+      <RoomGallerySlider />
+      <DeluxeRoomDetail />
     </div>
   )
 }
